96bb2027cb2fd67c751ed99e3cd2bc0c213acf7c
Jason-Sus <jeremywang08@gmail.com>
2024-03-24 17:50:16 -0400

    Update CubesatCamera.py

--- CubesatCamera.py
+++ CubesatCamera.py
@@ -33,5 +33,5 @@ mag = LIS3MDL(i2c) #magnetometer
 
 #test code
 while(True):
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.acceleration))
-    # print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (gyro.gyro))
+    # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (gyro.gyro))
