0b20b063b45a2bf75bb715f547e0fda99fe4e0b8
Jason-Sus <jeremywang08@gmail.com>
2024-03-24 17:59:12 -0400

    camera teset

--- CubesatCamera.py
+++ CubesatCamera.py
@@ -18,20 +18,39 @@ import time
 import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
-from picamera2  import Picamera2
+from picamera2  import Picamera2, Preview
 
 #Git Setup
 from git import Repo
 
 repo = Repo("/home/massbuilders/sat")
-image_path = "/home/massbuilders/sat/images"
+image_path = "/images"
 
 #IMU Setup
 i2c = board.I2C()
 gyro = LSM6DS(i2c) #gyro+accel
 mag = LIS3MDL(i2c) #magnetometer
 
+#Camera Setup
+picam2=Picamera2()
+config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores") # adjust img size here
+picam2.configure(config)
+
 #test code
-while(True):
-    # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (gyro.gyro))
+
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
+
+
+
+
+
+
+
+
+
+# while(True):
+#     # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.acceleration))
+#     print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (gyro.gyro))
