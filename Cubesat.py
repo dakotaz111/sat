5d45ca8c37340eec2125237f974d8c24670fc27f
Jason-Sus <jeremywang08@gmail.com>
2024-03-26 20:08:36 -0400

    arthurlinisgay

--- Cubesat.py
+++ Cubesat.py
@@ -1,5 +1,67 @@
+"""
+MIT BWSI Cubesat Challenge
+MIT license
+
+File Name: CubesatCamera.py
+
+Title: Cubesat Camera
+
+Author: Jeremy Wang (MASSBUILDERS)
+
+Purpose: This code will interface all of the electrical components of the Cubesat and will first take a picture using a camera, then send the image to github
+"""
+
+#Imports
+import time
+from datetime import datetime
 import cv2
 import numpy as np
+import csv
+
+
+import board
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
+from adafruit_lis3mdl import LIS3MDL
+from picamera2  import Picamera2, Preview
+
+#Git Setup
+from git import Repo
+
+repo = Repo("/home/massbuilders/sat")
+
+black_coords = []
+white_coords = []
+
+#IMU Setup
+i2c = board.I2C()
+gyro = LSM6DS(i2c) #gyro+accel
+mag = LIS3MDL(i2c) #magnetometer
+
+#Camera Setup
+picam2=Picamera2()
+config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores") # adjust img size here
+picam2.configure(config)
+
+def git_push():
+    file.close()
+    origin = repo.remote('origin')
+    print('added remote')
+    origin.pull()
+    print('pulled changes')
+    repo.git.add("/home/massbuilders/sat/images")
+    repo.index.commit('New Photo')
+    print('made the commit')
+    origin.push()
+    print('pushed changes')
+    picam2.stop()
+
+def name():
+    now = datetime.now()
+    date = now.strftime("%m-%d-%Y_%H-%M-%S")
+    name = f"/home/massbuilders/sat/images/{date}.jpg"
+    return name
+
+name = name()
 
 #Script for determining the percentage of each color using PIL and using
 #RGB representation. When running in the command line, type the image file.
@@ -36,8 +98,8 @@ def processing(image):
     return (processedImg, black_coords, white_coords, perc_black, perc_white);
    
 #Main code that is being run
-def color_id(image_file = 'test.jpg'):
-    folder_path = '/home/pi/sat' #Replace with the folder path for the folder in the
+def color_id(image_file):
+    folder_path = f'/home/massbuilders/sat/images' #Replace with the folder path for the folder in the
                      #Flat Sat Challenge with your name so you can view images
                      #on Github if you don't have VNC/X forwarding
 
@@ -46,7 +108,23 @@ def color_id(image_file = 'test.jpg'):
     processedImg, black_coords, white_coords, perc_black, perc_white = processing(image)
     
     cv2.imwrite(folder_path + 'processedImg.jpg', processedImg)
+    git_push()
 
+#test code
+data = [black_coords]
+with open('processedData.csv','a', newline='') as file:
+    csvreader = csv.writer(file)
+    csvwriter.writerows(data)
+
+picam2.start()
+time.sleep(2)
+name = name()
+picam2.capture_file(name)
+picam2.stop()
+
+# while(True):
+#     # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (gyro.acceleration))
+#     print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (gyro.gyro))
 
 """ This code is for command line entry. It allows you to add arguments 
     for what you want the code to run on. For instance, if I want to run 
@@ -56,4 +134,4 @@ def color_id(image_file = 'test.jpg'):
 if __name__ == '__main__':
     import sys
     
-    color_id(*sys.argv[1:])
+    color_id(name)
